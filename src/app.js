92ca38aca39425a354ff8320f535e4b076edc202
BakhatMuneerDev <bakhatmuneer.webdev@gmail.com>
2025-02-21 19:55:52 +0500

    Add CORS and cookie-parser middleware, update package.json and package-lock.json

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors"
+
+
+const app = express();
+
+app.use(cors({
+    origin: CORSE_ORIGINE
+}))
+
+app.use(express.json({ limit: "15kb" }));  // Limit JSON size to 15kb
+app.use(express.urlencoded({extended: true, limit: "15kb"}))
+app.use(express.static("public"))
+
+
+export {app};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-const app = express()
 dotenv.config({
     path: "./.env"
 })
 
 connectDB()
+.then(
+    app.listen(process.env.PORT || 5000, (req, res)=>{
+        console.log(`app is running at PORT :: ${process.env.PORT}`)
+    })
+)
+.catch(
+    (error) => {
+        console.error("Faild to talk with mongodb", error)
+    }
+)
