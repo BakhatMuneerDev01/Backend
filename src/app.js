9f178e06a330a32698b42b7f9c355ce6c07917d9
BakhatMuneerDev <bakhatmuneer.webdev@gmail.com>
2025-03-03 11:21:01 +0500

    Implement user registration route and async error handler; update app configuration

--- src/app.js
+++ src/app.js
@@ -5,12 +5,18 @@ import cors from "cors"
 const app = express();
 
 app.use(cors({
-    origin: CORSE_ORIGINE
+    origin: process.env.CORSE_ORIGINE
 }))
 
 app.use(express.json({ limit: "15kb" }));  // Limit JSON size to 15kb
-app.use(express.urlencoded({extended: true, limit: "15kb"}))
+app.use(express.urlencoded({ extended: true, limit: "15kb" }))
 app.use(express.static("public"))
 
+// import routes
+import  userRouter  from "../src/routes/user.route.js"
 
-export {app};
+// routes
+app.use("api/v1/users", userRouter)
+
+
+export { app };
